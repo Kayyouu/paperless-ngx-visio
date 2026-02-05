42f2cd89322d2b1108217af8bfc96a8a8500cbb2
Kayyouu <mathieu.beaureilles@proton.me>
2026-02-05 21:08:49 +0100

    Added paperless_visio app

--- src/paperless/settings.py
+++ src/paperless/settings.py
@@ -347,6 +347,7 @@ INSTALLED_APPS = [
     "paperless_text.apps.PaperlessTextConfig",
     "paperless_mail.apps.PaperlessMailConfig",
     "paperless_remote.apps.PaperlessRemoteParserConfig",
+    "paperless_visio.apps.PaperlessVisioConfig",
     "django.contrib.admin",
     "rest_framework",
     "rest_framework.authtoken",
